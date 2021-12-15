9537c2fb607f8e3f25611d92f367eb5cf86d01b7
nradkova <neliradkova@abv.bg>
2021-12-15 17:07:19 +0200

    pagination and custom hook for books page

--- src/pages/books/index.js
+++ src/pages/books/index.js
@@ -1,150 +1,130 @@
-import { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useContext } from "react";
 
 import './index.css';
 
 import AuthContext from "../../context/authContext";
-import { searchDataValidation } from "../../utils/validation";
-import { getAllBooks, getBooksByAuthor, getBooksByCategory, getBooksByCreator, getBooksByTitle } from "../../services/book";
+import useBookSearch from "../../hooks/useBookSearch";
 
 import Title from "../../components/title";
+import Loader from "../../components/loader";
 import PageLayout from "../../components/pageLayout";
 import BookCardMedium from "../../components/book-card-medium";
 import ValidationError from "../../components/validationError";
-import Loader from "../../components/loader";
 
 const Books = () => {
-  const { user } = useContext(AuthContext);
-  const [books, setBooks] = useState([]);
-  const [isLoading, setIsloading] = useState(false);
-  const [validationError, setValidationError] = useState(null);
-  const [searchMessage, setSearchMessage] = useState(null);
+	const { user } = useContext(AuthContext);
+	const {
+		query,
+		books,
+		pagination,
+		isLoading,
+		validationError,
+		searchMessage,
+		isDisabledIncreaseButton,
+		isDisabledDecreaseButton,
+		decreaseCounter,
+		increaseCounter,
+		onClickMyPostsHandler,
+		onClickAllPostsHandler,
+		onSubmitSearchHandler,
+		requestCountHandler,
+		requestDataHandler,
+	} = useBookSearch(user.username)
 
 
-  useEffect(() => {
-    getAllBooksHandler()
-  }, [])
+	useEffect(() => {
+		requestCountHandler();
+	}, [query]);
+
+	useEffect(() => {
+		if(pagination.count>0){
+			requestDataHandler();
+		}
+	}, [pagination]);
 
 
-  const getAllBooksHandler = async () => {
-    const res = await getAllBooks();
-    console.log(res);
-    setBooks(res)
-    // setBooks([])
+	const searchMyPosts = (
+		<div className="search-my-posts">
+			<div className="search-my-posts-icon">
+				<i className="far fa-calendar-check"></i>
+				<span className="search-my-posts-action" onClick={onClickMyPostsHandler} >MY BOOKS</span>
+			</div>
+		</div>
+	)
 
-  }
+	const searchAllPosts = (
+		<div className="search-all-posts">
+			<div className="search-all-posts-icon">
+				<i className="far fa-calendar-check"></i>
+				<span className="search-all-posts-action" onClick={onClickAllPostsHandler} >ALL BOOKS</span>
+			</div>
+		</div>
+	)
 
-  const onSubmitSearchHandler = async (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const search = formData.get('search');
-    const criteria = formData.get('criteria');
-	  setBooks([]);
-	  const error=searchDataValidation(criteria, search)
-    setValidationError(error);
-    if (error) {
-      return;
-    }
+	const searchResults = (
+		<>
+			{searchMessage.criteria && <p className="search-message">Found &nbsp; <strong>{pagination.count}</strong>	&nbsp; search results for	&nbsp; <strong>{searchMessage.search}</strong>	&nbsp; in 	&nbsp;<strong>{searchMessage.criteria}</strong>	&nbsp;</p>}
+			{query.all && <p className="search-message">Latests books...</p>}
+			<div className="books-container search">
+				<button className="prev-books" disabled={isDisabledDecreaseButton} onClick={decreaseCounter}>&#10094;</button>
+				<div className="books-inner-container">
+					{books.map(x => <BookCardMedium key={x.id} id={x.id} imageUrl={x.imageUrl} title={x.title} author={x.author} rating={x.rating} />)}
+				</div>
+				<button className="next-books" disabled={isDisabledIncreaseButton} onClick={increaseCounter}>&#10095;</button>
+			</div>
+			<p className="search-pages">{(pagination.totalPages===0 ?pagination.counter-1:pagination.counter)}/{pagination.totalPages}</p>
+		</>
+	)
 
-    setIsloading(true);
-
-    if (criteria === 'postedBy') {
-      const data = await getBooksByCreator(search);
-      setIsloading(false);
-      setBooks(data);
-      setSearchMessage({ criteria: 'users', search });
-    }
-    if (criteria === 'category') {
-      const data = await getBooksByCategory(search);
-      setIsloading(false);
-      setBooks(data);
-      setSearchMessage({ criteria: 'books categories', search });
-    }
-    if (criteria === 'author') {
-      const data = await getBooksByAuthor(search);
-      setIsloading(false);
-      setBooks(data);
-      setSearchMessage({ criteria: 'books authors', search });
-    }
-    if (criteria === 'title') {
-      const data = await getBooksByTitle(search);
-      setIsloading(false);
-      setBooks(data);
-      setSearchMessage({ criteria: 'books titles', search });
-    }
-    e.target.reset();
-  }
-
-  const onClickMyPostsHandler = async () => {
-    setIsloading(true);
-    const data = await getBooksByCreator(user.username);
-    setIsloading(false);
-    setBooks(data);
-    setSearchMessage({ criteria: user.username +' profile', search:'books' });
-  }
-
-  const searchMyPosts = (
-    <div className="search-my-posts">
-      <div className="search-my-posts-icon">
-        <i className="far fa-calendar-check"></i>
-        <span className="search-my-posts-action" onClick={onClickMyPostsHandler} >MY BOOKS</span>
-      </div>
-    </div>
-  )
-
-  return (
-    <PageLayout>
-      <Title title="Books" />
-      {user.username && searchMyPosts}
-      <div className="all-books-container">
-        <div className="search-container">
-          <form action="" onSubmit={onSubmitSearchHandler}>
-            <input className="main-input" type="text" placeholder="Search by key and criteria..." name="search" />
-            <div className="search-options-list">
-              <div className="search-option popup-title">
-                <label className="search-options-label-title">
-                  <input type="radio" className="popup-title" name="criteria" defaultValue="title" />
-                  <i className="far fa-bookmark"></i>
-                  <span className="popuptext-title">BY TITLE</span>
-                </label>
-              </div>
-              <div className="search-option popup-author">
-                <label className="search-options-label-author">
-                  <input type="radio" className="popup-author" name="criteria" defaultValue="author" />
-                  <i className="far fa-edit"></i>
-                  <span className="popuptext-author">BY AUTHOR</span>
-                </label>
-              </div>
-              <div className="search-option popup-category">
-                <label className="search-options-label-category">
-                  <input type="radio" className="popup-category" name="criteria" defaultValue="category" />
-                  <i className="fas fa-list-ul"></i>
-                  <span className="popuptext-category">BY CATEGORY</span>
-                </label>
-              </div>
-              <div className="search-option popup-postedBy">
-                <label className="search-options-label-postedBy">
-                  <input type="radio" className="popup-postedBy" name="criteria" defaultValue="postedBy" />
-                  <i className="far fa-id-badge"></i>
-                  <span className="popuptext-postedBy">POSTED BY</span>
-                </label>
-              </div>
-            </div>
-            <button className="search-btn" type="submit"> <i className="fa fa-search"></i></button>
-          </form>
-          {validationError && <ValidationError message={validationError} />}
-        </div>
-        {isLoading ? <Loader /> : null}
-        {searchMessage
-          ? <p className="search-message">Found &nbsp; <strong>{books.length}</strong>	&nbsp; search results for	&nbsp; <strong>{searchMessage.search}</strong>	&nbsp; in 	&nbsp;<strong>{searchMessage.criteria}</strong>	&nbsp;</p>
-          : null
-        }
-        <div className="books-inner-container">
-          {books.map(x => <BookCardMedium key={x.id} id={x.id} imageUrl={x.imageUrl} title={x.title} author={x.author} rating={x.rating} />)}
-        </div>
-      </div>
-    </PageLayout>
-  )
+	return (
+		<PageLayout>
+			<Title title="Books" />
+			<div className="search-posts">
+			{searchAllPosts}
+			{user.username && searchMyPosts}
+			</div>
+			<div className="all-books-container">
+				<div className="search-container">
+					<form action="" onSubmit={onSubmitSearchHandler}>
+						<input className="main-input" type="text" placeholder="Search by key and criteria..." name="search" />
+						<div className="search-options-list">
+							<div className="search-option popup-title">
+								<label className="search-options-label-title">
+									<input type="radio" className="popup-title" name="criteria" defaultValue="title" />
+									<i className="far fa-bookmark"></i>
+									<span className="popuptext-title">BY TITLE</span>
+								</label>
+							</div>
+							<div className="search-option popup-author">
+								<label className="search-options-label-author">
+									<input type="radio" className="popup-author" name="criteria" defaultValue="author" />
+									<i className="far fa-edit"></i>
+									<span className="popuptext-author">BY AUTHOR</span>
+								</label>
+							</div>
+							<div className="search-option popup-category">
+								<label className="search-options-label-category">
+									<input type="radio" className="popup-category" name="criteria" defaultValue="category" />
+									<i className="fas fa-list-ul"></i>
+									<span className="popuptext-category">BY CATEGORY</span>
+								</label>
+							</div>
+							<div className="search-option popup-postedBy">
+								<label className="search-options-label-postedBy">
+									<input type="radio" className="popup-postedBy" name="criteria" defaultValue="postedBy" />
+									<i className="far fa-id-badge"></i>
+									<span className="popuptext-postedBy">POSTED BY</span>
+								</label>
+							</div>
+						</div>
+						<button className="search-btn" type="submit"> <i className="fa fa-search"></i></button>
+					</form>
+					{validationError && <ValidationError message={validationError} />}
+				</div>
+				{isLoading ?<Loader/> :searchResults}
+			</div>
+		</PageLayout>
+	)
 }
 
 export default Books;
