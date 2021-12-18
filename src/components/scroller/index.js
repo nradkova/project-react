592c388acbbf04060165ab63244d92c6bd25517e
nradkova <neliradkova@abv.bg>
2021-12-18 10:49:34 +0200

    scroller setVisible fixed

--- src/components/scroller/index.js
+++ src/components/scroller/index.js
@@ -4,16 +4,23 @@ import './index.css'
 
 const Scroller = () => {
   const [isSelected, setIsSelected] = useState('');
-  
+
   const onClickHandler = (e) => {
     window.scrollTo(0, 0)
   }
- 
-  window.addEventListener('scroll', () => {
+
+  const setVisible = () => {
     if (window.innerHeight + 30 >= window.scrollY) {
       setIsSelected('selected');
     }
-  });
+  }
+  window.addEventListener('scroll', setVisible);
+
+  window.addEventListener('scroll', () => {
+    if (window.innerHeight + 50 >= window.scrollY) {
+      window.removeEventListener('scroll', setVisible);
+    }
+  })
 
   return (
     <button className={`scroller-button ${isSelected}`} onClick={onClickHandler}><i className="fas fa-arrow-up"></i></button>
